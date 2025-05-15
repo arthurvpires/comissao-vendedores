81b9cdf19699af08185354c2dd62a987d4422948
Arthur <arthurvpires@live.com>
2025-05-15 11:26:18 -0300

    Install Tailwind CSS

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,vue}", // Adiciona todos os arquivos Vue e TypeScript que você tem
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
